cb6393972531e40cfddb66f827e7880b46a4ded9
bosko.kojic <bosko.kojic@LT-BKOJIC>
2023-01-26 17:02:52 +0100

    Initial changes to insert routed navigation, hero section and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import React from 'react';
 import './App.css';
+
+import Home from "./routes/Home";
+import NoPage from "./components/NoPage";
+/*
+import About from "./routes/About";
+import Login from "./routes/Login";
+import Menu from "./routes/Menu";
+import Reservations from "./routes/Reservations";
+import OrderOnline from "./routes/OrderOnline";
+*/
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useRoutes,
+  BrowserRouter,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      {/* posto ima "/", ovo se renderuje po difoltu   */}
+      <Route path = "/" element = {<Home />}/>
+      <Route path="*" element={<NoPage />} />
+      {/*
+      <Route path = "/about" element = {<About />}/>
+      <Route path = "/login" element = {<Login />}/>
+      <Route path = "/menu" element = {<Menu />}/>
+      <Route path = "/order-online" element = {<OrderOnline />}/>
+      <Route path = "/reservations" element = {<Reservations />}/>
+      */}
+    </Routes>
+    </BrowserRouter>
+</>
+     
   );
 }
 
